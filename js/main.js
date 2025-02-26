609dd4de28b6d2301efa34d2657f92943600cb70
Luidy Teles Araujo <luidyteles620@gmail.com>
2025-02-26 17:08:50 -0300

    FIX - Melhorando aparencia

--- js/main.js
+++ js/main.js
@@ -1,83 +1,101 @@
-
 function main() {
+  (function () {
+    "use strict";
 
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
+    // Rolagem suave ao clicar nos links de navegação
+    $("a.page-scroll").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top - 40,
+            },
+            900
+          );
+          return false;
         }
+      }
+    });
+
+    // Mostrar o menu quando a rolagem atingir uma certa altura
+    $(window).bind("scroll", function () {
+      var navHeight = $(window).height() - 500;
+      if ($(window).scrollTop() > navHeight) {
+        $(".navbar-default").addClass("on");
+      } else {
+        $(".navbar-default").removeClass("on");
+      }
+
+      // Mostrar/ocultar a seta de voltar ao topo
+      if ($(this).scrollTop() > 100) {
+        // Se o usuário rolar mais de 100px
+        $("#backToTop").addClass("show");
+      } else {
+        $("#backToTop").removeClass("show");
+      }
+    });
+
+    // Configuração do scrollspy
+    $("body").scrollspy({
+      target: ".navbar-default",
+      offset: 80,
+    });
+
+    // Fechar o menu ao clicar em um item de navegação
+    $(".navbar-nav li a").click(function (event) {
+      // Verifica se a janela é pequena o suficiente para o menu suspenso
+      var toggle = $(".navbar-toggle").is(":visible");
+      if (toggle) {
+        $(".navbar-collapse").collapse("hide");
+      }
+    });
+
+    // Filtro do portfólio com Isotope
+    $(window).load(function () {
+      var $container = $(".portfolio-items");
+      $container.isotope({
+        filter: "*",
+        animationOptions: {
+          duration: 750,
+          easing: "linear",
+          queue: false,
+        },
       });
-
-	
-    // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 500;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    });
-
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-	
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
+      $(".cat a").click(function () {
+        $(".cat .active").removeClass("active");
+        $(this).addClass("active");
+        var selector = $(this).attr("data-filter");
         $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: false,
+          },
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
+        return false;
+      });
     });
-	
-    // Nivo Lightbox 
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
-        });
 
-}());
+    // Nivo Lightbox para o portfólio
+    $(".portfolio-item a").nivoLightbox({
+      effect: "slideDown",
+      keyboardNav: true,
+    });
 
-
+    // Rolagem para o topo ao clicar na seta
+    $("#backToTop").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+  })();
 }
 main();
